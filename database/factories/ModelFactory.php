43a484f416da5b33c7e638aecc60c120cc7d8212
Thuan <ndthuanuet@gmail.com>
2017-07-24 18:06:58 +0700

    init backend chatbox

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,13 +12,48 @@
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+
+$factory->define(App\Admin::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'phone' => $faker->phoneNumber,
+        'password' => $password ?: $password = bcrypt('1'),
         'remember_token' => str_random(10),
+        'created_at' => $faker-> dateTimeBetween('-2 months', 'now')
+    ];
+});
+
+$factory->define(App\Room::class, function (Faker\Generator $faker) {
+    static $array_room = ["Support", "Sale", "Other"];
+
+    return [
+        'type' => $faker->randomElement($array_room),
+        'created_at' => $faker-> dateTimeBetween('-2 months', 'now')
+    ];
+});
+
+$factory->define(App\Customer::class, function (Faker\Generator $faker) {
+
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'phone' => $faker->phoneNumber,
+        'created_at' => $faker-> dateTimeBetween('-2 months', 'now')
+    ];
+});
+
+$factory->define(App\Message::class, function (Faker\Generator $faker) {
+    static $password;
+
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'phone' => $faker->phoneNumber,
+        'password' => $password ?: $password = bcrypt('1'),
+        'remember_token' => str_random(10),
+        'created_at' => $faker-> dateTimeBetween('-2 months', 'now')
     ];
 });
