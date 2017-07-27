bb81c031279e12f93cc165b8afb7cd6216fa8f1e
Đỗ Quốc Vương <vuongtlt13@gmail.com>
2017-07-27 11:31:17 +0700

    init2

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -13,7 +13,6 @@
                 <div class="row">
                     <div class="col-sm-12">
                         <div class="btn-group pull-right m-t-15">
-                            <button type="button" class="btn btn-default dropdown-toggle waves-effect waves-light" data-toggle="dropdown" aria-expanded="false">Settings <span class="m-l-5"><i class="fa fa-cog"></i></span></button>
                             <ul class="dropdown-menu drop-menu-right" role="menu">
                                 <li><a href="#">Action</a></li>
                                 <li><a href="#">Another action</a></li>
