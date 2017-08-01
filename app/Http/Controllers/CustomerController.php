1a088e76270db1d0bc9d7f7c6d99353f2f1ecae2
Đỗ Quốc Vương <vuongtlt13@gmail.com>
2017-08-01 14:34:23 +0700

    ajax customer

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -6,6 +6,7 @@ use App\Message;
 use Illuminate\Http\Request;
 use App\Customer;
 use Illuminate\Support\Facades\DB;
+use Yajra\Datatables\Facades\Datatables;
 
 class CustomerController extends Controller
 {
@@ -14,19 +15,20 @@ class CustomerController extends Controller
         $this->middleware('authenticate');
     }
     public function index(Request $request) {
-        $data = [];
-        $customers = Customer::all();
-        foreach ($customers as $customer) {
-            $rooms = $this->getRooms($customer->id);
-            $sentMessages = $this->getSentMessages($customer->id);
-            $element = array($customer, $rooms, $sentMessages);
-            $data[] = $element;
-        }
-
-//        foreach ($data as $d) {
-//            echo $d[0]->id . PHP_EOL;
+//        $data = [];
+//        $customers = Customer::all();
+//        foreach ($customers as $customer) {
+//            $rooms = $this->getRooms($customer->id);
+//            $sentMessages = $this->getSentMessages($customer->id);
+//            $element = array($customer, $rooms, $sentMessages);
+//            $data[] = $element;
 //        }
-        return view('customer.customer')->with('data', $data);
+//
+////        foreach ($data as $d) {
+////            echo $d[0]->id . PHP_EOL;
+////        }
+        //dd($this->getRooms(1));
+        return view('customer.customer');
     }
 
     public function add(Request $request) {
@@ -82,18 +84,52 @@ class CustomerController extends Controller
 
 
     public function getRooms($id){
-        $numberOfRooms = DB::table('messages')->select(DB::raw('count(room_id) as Total'))
-            ->where('sender_id', $id)
-            ->groupBy('room_id')
-            ->get();
-
-        $res = sizeof($numberOfRooms);
-        return $res;
+        //select count(*) as count From
+        //(
+        //  select count(room_id) as Total
+        //  from messages
+        //  where sender_id=1
+        //  group by room_id
+        //) as sub;
+        $res = DB::select(DB::raw('select count(*) as count From (' .
+          'select count(room_id) as Total ' .
+          'from messages ' .
+          'where sender_id="1" ' .
+          'group by room_id) as sub'));
+        //$res = sizeof($numberOfRooms);
+        return $res[0]->count;
+//        return $numberOfRooms;
     }
 
     public function getSentMessages($id){
         $numberOfMessages = Message::where('sender_id', $id)
                             ->count();
         return $numberOfMessages;
+    }
+
+    public function getRoom(Request $request) {
+//        SELECT c.*, count(c.id)
+//            FROM customers as c, messages as m
+//            WHERE c.id = m.sender_id
+//            GROUP BY c.id
+//            ORDER BY c.id
+
+//        select c.*, count(distinct m.room_id) from customers as c
+//        left join messages as m on c.id = m.sender_id
+//        group by c.id;
+
+//        $tmpTable = DB::table('customers as c')
+//                        ->join('messages as m', 'c.id', '=', 'm.sender_id')
+//                        ->select('c.*', DB::raw('count(c.id)'))
+//                        ->groupBy('c.id')
+//                        ->orderBy('c.id');
+
+        $tmpTable2 = DB::table('customers as c')
+                        ->leftJoin('messages as m', 'c.id', '=', 'm.sender_id')
+                        ->select('c.*', DB::raw('count(distinct m.room_id) as rooms'), DB::raw('count(m.id) as sentMessages'))
+                        ->groupBy('c.id');
+
+        $customer = $tmpTable2;
+        return Datatables::of($customer)->make(true);
     }
 }
--- resources/views/customer/customer.blade.php
+++ resources/views/customer/customer.blade.php
@@ -50,33 +50,37 @@
                                 </tr>
                                 </thead>
 
-
                                 <tbody>
-                                @foreach($data as $d)
-                                    <tr id="{{$d[0]->id}}">
-                                        <td>{{$d[0]->id}}</td>
-                                        <td>{{$d[0]->name}}</td>
-                                        <td>{{$d[0]->phone}}</td>
-                                        <td>{{$d[0]->email}}</td>
-                                        @if(empty($d[0]->created_at))
-                                            <td>NULL</td>
-                                        @else
-                                            <td>{{$d[0]->created_at->toDateString()}}</td>
-                                        @endif
-                                        @if(empty($d[0]->updated_at))
-                                            <td>NULL</td>
-                                        @else
-                                            <td>{{$d[0]->updated_at->toDateString()}}</td>
-                                        @endif
-                                        <td>{{$d[1]}}</td>
-                                        <td>{{$d[2]}}</td>
-                                        <td>
-                                            <a href="#custom-modal" data-id ="{{$d[0]->id}}" data-overlayspeed="200" data-overlaycolor="#36404a" data-animation="fadein" data-plugin="custommodal" class="btn btn-default btn-sm waves-effect waves-light btn-edit">
-                                                <i class="fa fa-pencil"></i>
-                                            </a>
-                                        </td>
-                                    </tr>
-                                @endforeach
+                                {{--@foreach($data as $d)--}}
+                                    {{--<tr id="{{$d[0]->id}}">--}}
+                                        {{--<td id="customer-id">{{$d[0]->id}}</td>--}}
+                                        {{--<td>{{$d[0]->name}}</td>--}}
+                                        {{--<td>{{$d[0]->phone}}</td>--}}
+                                        {{--<td>{{$d[0]->email}}</td>--}}
+                                        {{--@if(empty($d[0]->created_at))--}}
+                                            {{--<td>NULL</td>--}}
+                                        {{--@else--}}
+                                            {{--<td>{{$d[0]->created_at->toDateString()}}</td>--}}
+                                        {{--@endif--}}
+                                        {{--@if(empty($d[0]->updated_at))--}}
+                                            {{--<td>NULL</td>--}}
+                                        {{--@else--}}
+                                            {{--<td>{{$d[0]->updated_at->toDateString()}}</td>--}}
+                                        {{--@endif--}}
+                                        {{--<td>{{$d[1]}}</td>--}}
+                                        {{--<td>{{$d[2]}}</td>--}}
+                                        {{--<td>--}}
+                                            {{--<a href="#custom-modal" data-id ="{{$d[0]->id}}" data-overlayspeed="200" data-overlaycolor="#36404a" data-animation="fadein" data-plugin="custommodal" class="btn btn-default btn-sm waves-effect waves-light btn-edit">--}}
+                                                {{--<i class="fa fa-pencil"></i>--}}
+                                            {{--</a>--}}
+                                        {{--</td>--}}
+                                    {{--</tr>--}}
+                                {{--@endforeach--}}
+                                {{--<td>--}}
+                                {{--<a href="#custom-modal" data-id ="{{$d[0]->id}}" data-overlayspeed="200" data-overlaycolor="#36404a" data-animation="fadein" data-plugin="custommodal" class="btn btn-default btn-sm waves-effect waves-light btn-edit">--}}
+                                {{--<i class="fa fa-pencil"></i>--}}
+                                {{--</a>--}}
+                                {{--</td>--}}
                                 </tbody>
                             </table>
                         </div>
@@ -95,6 +99,53 @@
             © 2017. All rights reserved.
         </footer>
 
+    </div>
+
+    <!--Modal-->
+    <div id="con-close-modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form role="form" class="form_modal" id="form_edit" method="post" action="/customers/edit?id=">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                        <h4 class="modal-title">Edit</h4>
+                    </div>
+                    <div class="modal-body">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        <input type="hidden" name="id" id="form_id" value="">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label for="name" class="control-label">Name</label>
+                                    <input type="text" class="form-control" id="editName" name="name" placeholder="Enter name">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label for="email" class="control-label">Email</label>
+                                    <input type="email" class="form-control" id="editEmail" name="email" placeholder="Enter email">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label for="phone">Phone number</label>
+                                    <input type="tel" class="form-control" id="editPhone" name="phone" placeholder="Enter phone number">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <span id="messageE"></span>
+                        <button type="button" class="btn btn-default waves-effect" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-info waves-effect waves-light">Save changes</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 
     <!-- Modal -->
@@ -138,10 +189,3 @@
 @section('script')
     <script src="/js/customer.js"></script>
 @endsection
-@push('inline_scripts')
-<script type="text/javascript">
-    $(document).ready(function () {
-        $('#datatable').dataTable();
-    });
-</script>
-@endpush
