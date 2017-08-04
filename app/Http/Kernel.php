560f307f8c48101e2158b221a399ffc6b9c1e358
Nguyễn Đức Thuần <ndthuanuet@gmail.com>
2017-08-05 02:04:06 +0700

    jwt

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,5 +57,6 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'authenticate' => \App\Http\Middleware\CheckLogin::class,
+        'jwt.auth' => \App\Http\Middleware\VerifyJWTToken::class,
     ];
 }
