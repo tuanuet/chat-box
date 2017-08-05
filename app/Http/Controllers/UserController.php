8a3d00e7b5b1b8141522f78c9010c9c07a5a4e4a
Nguyễn Đức Thuần <ndthuanuet@gmail.com>
2017-08-05 23:01:21 +0700

    jwt

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,74 +4,86 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use JWTAuth;
-use App\User;
 use App\Customer;
 use App\Room;
 use App\Message;
 use JWTAuthException;
 use Carbon\Carbon;
+use Config;
 class UserController extends Controller
 {
-    private $user;
-    public function __construct(User $user){
-        $this->user = $user;
+    private $customer;
+    public function __construct(Customer $customer){
+        $this->customer = $customer;
     }
 
     public function createToken(Request $request)
     {
-//        $user = $this->user->create([
+
+//        $customer = $this->customer->create([
 //            'name' => $request->get('name'),
 //            'email' => $request->get('email'),
 //            'password' => bcrypt($request->get('password'))
 //        ]);
 
-        /** get user data
+        /** get customer data
          * json request must be have full of field include: "name", "email", "phone", "topicId", "message"*/
         $credentials = $request->json()->all();
         //authenticate json from client
-        if (!$credentials || !$credentials['name'] || !$credentials['email'] || !$credentials['phone']
-            || !$credentials['topicId']) {
-           return response()->json(['Invalid input'],422);
+        if ($credentials == null ) {
+            $credentials = $request->only('name', 'email', 'phone', 'topicId', 'message');
+        }
+
+        $name = $credentials['name'];
+        $phone = $credentials['phone'];
+        $email = $credentials['email'];
+        $msg = $credentials['message'];
+        $topicId = $credentials['topicId'];
+
+        /** validate data*/
+        if(!$name || !$phone || !$email || !$msg || !$topicId) {
+            return response()->json(['error' => 'invalid input'], 422);
         }
 
         /** add new customer to db*/
-        $user = new Customer;
-        $user->name = $credentials['name'];
-        $user->phone = $credentials['phone'];
-        $user->email = $credentials['email'];
+        $customer = new Customer;
+        $customer->name = $name;
+        $customer->phone = $phone;
+        $customer->email = $email;
 
         /** create a new room */
         $room = new Room;
-        $room->topic_id = $credentials['topicId'];
+        $room->topic_id = $topicId;
         $room->status = 0;
         $room->assignee = 0;
         $room->created_at = Carbon::now();
 
 
-
-        if ($user->save() && $room->save()) {
+        if ($customer->save() && $room->save()) {
 
             /** create first message of customer*/
             $message = new Message;
             $message->room_id = $room->id;
-            $message->sender_id = $user->id;
-            $message->content = $credentials['message'];
+            $message->sender_id = $customer->id;
+            $message->content = $msg;
             $message->created_at = Carbon::now();
 
             if (!$message->save()) {
                 return response()->json(['failed_to_create_token_caused_message_save'], 422);
             }
 
+
             /**create token by 5 fields: customerId, customerName, customerEmail, customerPhone and roomId */
             $token = null;
+
             try {
-                if (!$token = JWTAuth::attempt([
-                    "customerId" => $user->id,
-                    "customerName" => $user->name,
-                    "customerPhone" => $user->phone,
-                    "customerEmail" => $user->email,
-                    "roomId" => $room->id]
-                )) {
+                Config::set('auth.providers.customers.model', Customer::class);
+                if (!$token = JWTAuth::fromUser($customer,[
+                    "customerName" => $customer->name,
+                    "customerPhone" => $customer->phone,
+                    "customerEmail" => $customer->email,
+                    "roomId" => $room->id
+                ])) {
                     return response()->json(['failed_to_create_token'], 500);
                 }
             } catch (JWTAuthException $e) {
@@ -84,7 +96,13 @@ class UserController extends Controller
 
     }
     public function getAuthUser(Request $request){
-        $user = JWTAuth::toUser($request->token);
-        return response()->json(['result' => $user]);
+
+        $data = JWTAuth::getPayload($request->input('token'));
+
+        return response()->json(['customerId' => $data['sub'],
+            'customerName' => $data['customerName'],
+            'customerEmail' => $data['customerEmail'],
+            'customerPhone' => $data['customerPhone'],
+            'roomId' => $data['roomId']]);
     }
 }
--- app/Http/Middleware/VerifyJWTToken.php
+++ app/Http/Middleware/VerifyJWTToken.php
@@ -3,6 +3,7 @@ namespace App\Http\Middleware;
 use Closure;
 use JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
+use Config;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 class VerifyJWTToken
 {
@@ -16,7 +17,8 @@ class VerifyJWTToken
     public function handle($request, Closure $next)
     {
         try{
-            $user = JWTAuth::toUser($request->input('token'));
+
+            $data = JWTAuth::getPayload($request->input('token'));
         }catch (JWTException $e) {
             if($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
                 return response()->json(['token_expired'], $e->getStatusCode());
