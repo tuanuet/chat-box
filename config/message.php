658ff7e5461ca928651519f00d29aa7688b37b52
Đỗ Quốc Vương <vuongtlt13@gmail.com>
2017-08-14 11:00:35 +0700

    fix code and refactor

--- config/message.php
+++ config/message.php
@@ -4,6 +4,9 @@
          'NOTIFICATION' => 101,
          'RATING' => 102,
          'IMAGE' => 103,
+         'PDF' => 104,
+         'WORD' => 105,
+         'EXCEL' => 106,
          'CLOSE_CHAT' => 900
      ]
  ]
