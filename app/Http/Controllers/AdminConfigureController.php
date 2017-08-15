2587f9fe26367fe98c5976cd199c2f3031278327
Nguyen Duc Thuan <ndthuanuet@gmail.com>
2017-08-15 14:06:06 +0700

    run shell to build webpack and generate bundle.js for client

--- app/Http/Controllers/AdminConfigureController.php
+++ app/Http/Controllers/AdminConfigureController.php
@@ -2,9 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\File;
 use Illuminate\Http\Request;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class AdminConfigureController extends Controller
 {
     //
+    public function runConfigure(Request $request)
+    {
+        $registers = $request->input('registers');
+        $topics = $request->input('topics');
+        $configData = \GuzzleHttp\json_encode(["register" => $registers,
+            "topics" => $topics]);
+        $fp = fopen('config.json', 'w');
+        fwrite($fp, $configData);
+        fclose($fp);
+
+        $moveFile = new Process("cp config.json ../node");
+        $moveFile->run();
+
+        if (!$moveFile->isSuccessful()) {
+            throw new ProcessFailedException($moveFile);
+        }
+
+
+        $serverNode = "../node";
+
+        $processBuildWebpack = new Process('webpack');
+
+        $processBuildWebpack->setWorkingDirectory($serverNode);
+        $processBuildWebpack->run();
+        if (!$processBuildWebpack->isSuccessful()) {
+            throw new ProcessFailedException($processBuildWebpack);
+        }
+
+        echo "webpack build successfully";
+
+        $processRunServerNode = new Process('npm start');
+        $processRunServerNode->setWorkingDirectory($serverNode);
+        $processRunServerNode->run();
+        if (!$processRunServerNode->isSuccessful()) {
+            echo $processRunServerNode->getOutput();
+        }
+        echo "run server successfully";
+        return "http://localhost:3000/dist/bundle.js";
+    }
 }
--- client_app/src/components/App.js
+++ client_app/src/components/App.js
@@ -1,13 +1,9 @@
 import React, {PropTypes} from 'react';
-import Header from './common/Header';
-import LeftBar from './common/Leftbar';
 
 class App extends React.Component {
     render() {
         return(
             <div className="wrapper">
-                <Header/>
-                <LeftBar/>
                 <div className="content-page">
                     <div className="content">
                         <div className="container">
