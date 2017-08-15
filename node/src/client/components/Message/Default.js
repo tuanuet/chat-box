305a4f43c8164e93a5f1733a592a4b5af0c6a7d0
Nguyễn Trọng Toàn <toannt.181@gmail.com>
2017-08-15 10:21:11 +0700

    fix display metabox and time

--- node/src/client/components/Message/Default.js
+++ node/src/client/components/Message/Default.js
@@ -25,9 +25,13 @@ export default class Default extends React.Component {
 function Metabox(props) {
     return (
         <div className="meta-box">
-            <div className="font-weight-bold">{props.metadata.title}</div>
-            <div>{props.metadata.description}</div>
-            <img src={props.metadata.image}/>
+            <div className="box-image">
+                <img src={props.metadata.image}/>
+            </div>
+            <div className="box-content">
+                <div className="font-weight-bold">{props.metadata.title}</div>
+                <div>{props.metadata.description}</div>
+            </div>
         </div>
     );
 }
